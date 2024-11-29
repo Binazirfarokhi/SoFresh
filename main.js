1887d21956b3c07a3b13f9130d1677cd4a58a9d5
Binazir <104298006+Binazirfarokhi@users.noreply.github.com>
2024-11-29 11:52:30 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-// let count = document.getElementById("count");
+let count = document.getElementById("count");
 // count.innerText = 5;
 // first store the data : 
 // varialbe is a way to store a data 
 // let count = 0; 
 // console.log(count);
-// let btn = document.getElementById("btn");
+let btn = document.getElementById("btn");
 // btn.addEventListener("click",increment )
 // function increment(){
 // console.log("you are clicking")
@@ -20,5 +20,30 @@
 
 // invoke or run the function 
 // summer()
+// let number = 0;
+// function calculate(){
+// number ++; 
 
 
+// three times call the function and each time will calculate the previous number and added to the second dtage
+// at the end if you add the console.log will help you to calculate all other amount at the same time. 
+
+// calculate();
+// calculate();
+// calculate();
+// console.log(number)
+
+
+let counter  = 0; 
+btn.addEventListener("click", add)
+function add(){
+    counter = counter +1 ;
+    count.innerText = counter;
+    console.log(count);
+}
+let save= document.getElementById("save");
+save.addEventListener("click", newSave )
+function newSave(){
+    // return counter;
+    console.log(counter);
+}
