1d1c82c610d8b4e779fc3caa2ac554bca9a8f8e6
Binazir <104298006+Binazirfarokhi@users.noreply.github.com>
2024-11-29 10:25:28 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+// let count = document.getElementById("count");
+// count.innerText = 5;
+// first store the data : 
+// varialbe is a way to store a data 
+// let count = 0; 
+// console.log(count);
+// let btn = document.getElementById("btn");
+// btn.addEventListener("click",increment )
+// function increment(){
+// console.log("you are clicking")
+// }
+// let lap1 = 34
+// let lap2  = 45
+// let lap3 = 65
+// let sum;
+// function summer(){
+//    sum = lap1 + lap2 + lap3;
+//    console.log(sum)
+// }
+
+// invoke or run the function 
+// summer()
+
 
